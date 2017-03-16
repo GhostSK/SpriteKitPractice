1408ce9c5b5084411bffde87398a244d08ae6ee4
GhostSK <496774974@qq.com>
2017-03-16 18:18:21 +0800

    关键帧控制

--- particlesystem/particlesystem/GameScene.swift
+++ particlesystem/particlesystem/GameScene.swift
@@ -10,6 +10,7 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var emitterNode = SKEmitterNode()
     override func didMove(to view: SKView) {
 //        let snow = SKEmitterNode(fileNamed: "MyParticle.sks")
 //        snow?.position = CGPoint(x: 400, y: 600)
@@ -17,7 +18,39 @@ class GameScene: SKScene {
         let background:SKSpriteNode = SKSpriteNode(imageNamed: "1.png")
         background.position = CGPoint(x: 500, y: 380)
         addChild(background)
+        
+        //使用关键帧序列配置粒子属性
+        let rainTexture = SKTexture(imageNamed: "rainDrop.png")
+        emitterNode.particleTexture = rainTexture
+        emitterNode.particleBirthRate = 80.0
+        emitterNode.particleColor = SKColor.white
+        emitterNode.particleSpeed = -450
+        emitterNode.particleSpeedRange = 150
+        emitterNode.particleLifetime = 2
+        emitterNode.particleScale = 0.2
+        emitterNode.particleAlpha = 0.75
+        emitterNode.particleAlphaRange = 0.5
+        emitterNode.particleColorBlendFactor = 1
+        emitterNode.particleScale = 0.2
+        emitterNode.particleScaleRange = 0.5
+        emitterNode.particleScaleSpeed = -0.1
+        //设置发射器位置
+        emitterNode.position = CGPoint(x: (self.view?.frame.midX)!, y: (self.view?.frame.height)! + 10)
+        emitterNode.particlePositionRange = CGVector(dx: 736, dy: 0)
+        addChild(emitterNode)
+        
+        let btn = UIButton(frame: CGRect(x: 20, y: 30, width: 160, height: 30))
+        btn.setTitle("关键帧控制", for: .normal)
+        btn.addTarget(self, action: #selector(btnAction), for: .touchUpInside)
+        self.view?.addSubview(btn)
+        
     }
+    
+    func btnAction() {
+        let scaleSequence:SKKeyframeSequence = SKKeyframeSequence(keyframeValues: [0.2,0.7,0.1], times: [0.0,0.250,0.750]) //每个粒子会执行 时间0-scale0.2  时间0.250-scale0.7   时间0.750-scale0.1的顺序变化
+        emitterNode.particleScaleSequence = scaleSequence
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let rainTexture = SKTexture(imageNamed: "snow.png")
         let emitterNode = SKEmitterNode()
