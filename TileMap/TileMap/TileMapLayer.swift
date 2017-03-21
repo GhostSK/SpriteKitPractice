98d77a83c8b6714f97d7520fcc51ca553b41b1a8
GhostSK <496774974@qq.com>
2017-03-21 18:21:13 +0800

    瓦片地图

---
+++ TileMap/TileMap/TileMapLayer.swift
@@ -0,0 +1,71 @@
+//
+//  TileMapLayer.swift
+//  TileMap
+//
+//  Created by 胡杨林 on 17/3/20.
+//  Copyright © 2017年 胡杨林. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class TileMapLayer : SKNode {
+    var tileSize: CGSize = CGSize()   //用来保存瓦片的大小
+    var atlas: SKTextureAtlas? = nil  //用来保存纹理集合
+    
+    init(tilesize: CGSize) {
+        super.init()
+        self.tileSize = tilesize
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(atlasName: String, tileSize: CGSize, tileCodes: [String]) {
+        self.init(tilesize: tileSize)
+        atlas = SKTextureAtlas(named: atlasName)
+        //遍历
+        for row in 0..<tileCodes.count {
+            let line = tileCodes[row] as String
+//            for char in line {
+//                
+//            }
+        }
+    }
+    
+    //获取对应的瓦片
+    func nodeForCode(tileCode:Character) -> SKNode? {
+        //判断atlas是否为空
+        if atlas == nil{
+            return nil
+        }
+        var tile: SKNode?
+        //判断tileCode
+        switch tileCode {
+            
+        case "x":
+            tile = SKSpriteNode(texture: atlas!.textureNamed("wall"))
+            break
+        case "o":
+            tile = SKSpriteNode(texture: atlas!.textureNamed("grass"))
+            break
+        default:
+            print("未知图片")
+            break
+        }
+        if let sprite = tile as? SKSpriteNode {
+            sprite.blendMode = .replace
+        }
+        return tile
+    }
+    
+    func position(ForRow:Int, col: Int) ->CGPoint {
+        let x = CGFloat(col) * tileSize.width + tileSize.width / 2
+        let y = CGFloat(ForRow) * tileSize.height + tileSize.height / 2
+        return CGPoint(x: x, y: y)
+    }
+    
+    
+    
+}
